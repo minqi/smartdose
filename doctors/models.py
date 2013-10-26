bcf08d7589f9e9999795fc9a577e0d356bb4b021
Matthew Gaba <matthew.gaba.2@gmail.com>
2013-10-26 16:24:48 -0700

    Updated doctor with manager to add doctors

--- doctors/models.py
+++ doctors/models.py
@@ -1,7 +1,23 @@
 from django.db import models
+from django.db import transaction
 from common.models import UserProfile
 
-# Create your models here.
+class DoctorProfileManager(models.Manager):
+	"""Manager for performing operations on DoctorProfile records"""
+	@transaction.commit_on_success
+	def addDoctor(self, phone_number, first_name, last_name, primary_contact,
+				address_line1, address_line2, postal_code, city, state_province, country_iso_code,
+				email="", password=""):
+		user_profile = UserProfile.objects.addUser(phone_number, first_name, last_name, 
+										   primary_contact, 'd', #'d' is the user_type for doctor
+										   address_line1, address_line2, postal_code, 
+										   city, state_province, country_iso_code, 
+										   email, password)
+		doctor_profile = DoctorProfile.objects.create(user_profile=user_profile)
+		return doctor_profile
+
 class DoctorProfile(models.Model):
 	"""Model for doctor-specific information"""
+	#TODO(mgaba): Add doctor specific information: credentials, title, specialty
 	user_profile = models.OneToOneField(UserProfile)
+	objects = DoctorProfileManager()
