3fe0a3f78e7a60551e99c3d578f7a389ed8d7f51
Minqi Jiang <mnqjng@gmail.com>
2014-01-29 23:44:40 -0800

    add prescriptions for testing

--- .populatefakedata.py
+++ .populatefakedata.py
@@ -22,7 +22,8 @@ lucid_dream = Drug.objects.create(name="lucid dream check")
 
 # Create a patient Matt who takes a vitamin once a day in the afternoon and meditation twice a day. He also gets two lucid dream reminders.
 
-matt = PatientProfile.objects.get(primary_phone_number="2147094720")
+matt = PatientProfile.objects.create(primary_phone_number="2147094720")
+minqi = PatientProfile.objects.create(primary_phone_number="8569067308")
 
 matt_prescription1 = Prescription.objects.create(prescriber=bob, patient=matt, drug=meditation,
 												 note="To make you stable", safety_net_on=True)
@@ -31,7 +32,11 @@ matt_prescription2 = Prescription.objects.create(prescriber=bob, patient=matt, d
 matt_prescription3 = Prescription.objects.create(prescriber=bob, patient=matt, drug=lucid_dream,
 												 note="To make you lucid dream", safety_net_on=True)
 
+minqi_prescription3 = Prescription.objects.create(prescriber=bob, patient=minqi, drug=lucid_dream,
+												 note="To make you lucid dream", safety_net_on=True)
+
 matt_reminder3 = ReminderTime.objects.create(prescription=matt_prescription3, repeat=ReminderTime.DAILY, send_time=time(hour=17, minute=15))
+minqi_reminder3 = ReminderTime.objects.create(prescription=minqi_prescription3, repeat=ReminderTime.DAILY, send_time=time(hour=23, minute=59))
 
 
 
